dbff5244dee64c8720e0f60af32cef62d694ad36
JorgeRZT <georgeforceone@gmail.com>
2017-08-03 09:59:24 +0200

    Actualizacion

--- BuenasPracticas/Program.cs
+++ BuenasPracticas/Program.cs
@@ -6,12 +6,19 @@ namespace BuenasPracticas
 {
     class Program
     {
+        /// <summary>
+        /// Nombres de funciones en PascalCase
+        /// </summary>
+        void NombreEnPascalCase()
+        {
+
+        }
 
         /// <summary>
         /// Las excepciones siempre al principio
         /// </summary>
         /// <param name="objetos"></param>
-        void codigoExcepcion(List<Object> objetos)
+        void CodigoExcepcion(List<Object> objetos)
         {
             if (objetos == null) throw new Exception("Las excepcoines al principio");
         }
@@ -21,7 +28,7 @@ namespace BuenasPracticas
         /// </summary>
         /// <param name="cadenas"></param>
         /// <returns></returns>
-        string dividirList(List<String> cadenas)
+        string DividirList(List<String> cadenas)
         {
             return string.Join(",",cadenas.Select(c => c).ToArray());
         }
@@ -40,7 +47,7 @@ namespace BuenasPracticas
         /// Ejemplos LINQ
         /// </summary>
         /// <param name="objetos"></param>
-        void ejemploLINQ(List<Object> objetos)
+        void EjemploLINQ(List<Object> objetos)
         {
             //Select
             var tipos = objetos.Select(c => c.GetType().Equals("Tipo Requerido"));
@@ -52,7 +59,7 @@ namespace BuenasPracticas
         /// <summary>
         /// Hacer funciones para cada tipo de necesidad, sin agrupar muchas funcionalidades por funcion
         /// </summary>
-        void calculardora()
+        void Calculadora()
         {
             int a = 1;
             int b = 2;
